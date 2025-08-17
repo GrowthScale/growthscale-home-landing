b22a40b0b343eed15a8cd7ff3ba744aecb8d5585
wsmagalhaes <appsafestride@gmail.com>
2025-08-16 21:38:54 -0300

    feat: AuthProvider restaurado - autenticação ativa

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/contexts/AuthContext";
 
 // Importar apenas a página principal por enquanto
 import Index from "@/pages/Index";
 
 const App = () => {
-  console.log('App.tsx: SIMPLIFIED VERSION - React loaded:', typeof React);
+  console.log('App.tsx: AUTH PROVIDER RESTORED - React loaded:', typeof React);
   
   return (
     <React.StrictMode>
-      <BrowserRouter>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<Index />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="*" element={<Index />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
     </React.StrictMode>
   );
 };
